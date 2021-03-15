3996ecbb8883edef53bbcb3e9453be21fce72aef
Romulo Franca <romulo@cloudthings.dev>
2021-03-15 15:49:27 -0300

    update navbar to hide toggle dark button

--- components/Container.js
+++ components/Container.js
@@ -99,54 +99,54 @@ export default function Container(props) {
         >
           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
             <NextLink href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-2 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-200 hover:text-black sm:p-4 text-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 self-end">
+              <a className="lg:inline-flex lg:w-auto w-full px-2 py-2 ml-2 rounded text-white font-bold items-center justify-center hover:bg-gray-200 hover:text-black sm:p-2 text-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 self-end">
                 Home
               </a>
             </NextLink>
             <NextLink href="/snippets">
-              <a className="lg:inline-flex lg:w-auto w-full px-2 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-200 hover:text-black p-1 sm:p-4 text-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 self-end">
+              <a className="lg:inline-flex lg:w-auto w-full px-2 py-2 ml-2 rounded text-white font-bold items-center justify-center hover:bg-gray-200 hover:text-black p-1 sm:p-2 text-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 self-end">
                 Snippets
               </a>
             </NextLink>
             <NextLink href="/about">
-              <a className="lg:inline-flex lg:w-auto w-full px-2 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-200 hover:text-black p-1 sm:p-4 text-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 self-end">
+              <a className="lg:inline-flex lg:w-auto w-full px-2 py-2 ml-2 rounded text-white font-bold items-center justify-center hover:bg-gray-200 hover:text-black p-1 sm:p-2 text-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 self-end">
                 About
               </a>
             </NextLink>
+            <button
+              aria-label="Toggle Dark Mode"
+              type="button"
+              className="bg-gray-200 dark:bg-gray-800 rounded ml-2 mt-2 p-3 h-10 w-10 focus:outline-none"
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            >
+              {mounted && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  className="h-4 w-4 text-gray-800 dark:text-gray-200"
+                >
+                  {theme === 'dark' ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  )}
+                </svg>
+              )}
+            </button>
           </div>
         </div>
-        <button
-          aria-label="Toggle Dark Mode"
-          type="button"
-          className="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10 focus:outline-none"
-          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-        >
-          {mounted && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              stroke="currentColor"
-              className="h-4 w-4 text-gray-800 dark:text-gray-200"
-            >
-              {theme === 'dark' ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              )}
-            </svg>
-          )}
-        </button>
       </nav>
       <main
         id="skip"
